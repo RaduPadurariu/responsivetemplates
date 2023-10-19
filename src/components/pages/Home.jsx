dd76735ca5867c87746d8a4758138c2f7e748618
radupadurariu <padurariuionutradu@gmail.com>
2023-10-19 16:12:44 +0300

    add contact to Portfolio

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Home.css'
 import {Navbar} from './Navbar/Navbar'
 import { Template } from './Template'
+import { Link } from 'react-router-dom'
 
 export const Home = () => {
   return (
@@ -13,7 +14,10 @@ export const Home = () => {
         <Template cl='financialBackground' name="FINANCIAL TEMPLATE" link="/financial" title="FINANCIAL TEMPLATE" path="/images/Financial.png"/>
         <Template cl='hotelBackground' name="HOTEL TEMPLATE" link="/hotel/home" title="HOTEL TEMPLATE" path="/images/hotel.png"/>
       </div>
-      <div className='contact' id='contact'>&copy; copyright - Radu Padurariu 2023</div>
+      <Link to={'https://radupadurariu.netlify.com'} title='Radu Padurariu Portfolio' target='_blank'>
+        <div className='contact' id='contact'>&copy; copyright - Radu Padurariu 2023</div>
+      </Link>
+      
      
     </div>
   )
